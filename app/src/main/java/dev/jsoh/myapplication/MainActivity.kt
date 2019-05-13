9ea5c639640333404bf7b21deb1e7dcdebccf7b0
Junsuk Oh <a811219@gmail.com>
2019-05-13 23:36:21 +0900

    리팩토링

--- app/src/main/java/dev/jsoh/myapplication/MainActivity.kt
+++ app/src/main/java/dev/jsoh/myapplication/MainActivity.kt
@@ -1,18 +1,16 @@
 package dev.jsoh.myapplication
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.*
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
-import dev.jsoh.myapplication.databinding.ItemTodoBinding
 import dev.jsoh.myapplication.models.Todo
+import dev.jsoh.myapplication.ui.TodoAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
+        // Lottie 애니메이션 클릭시 사라지게
         animation_view.setOnClickListener {
             it.visibility = View.GONE
         }
@@ -74,47 +73,6 @@ class MainActivity : AppCompatActivity() {
         add_button.setOnClickListener {
             viewModel.insert(Todo(text = new_todo_edit.text.toString()))
             new_todo_edit.setText("")
-        }
-    }
-}
-
-class TodoAdapter(
-    private val clickListener: (item: Todo) -> Unit,
-    private val dragListener: (viewHolder: RecyclerView.ViewHolder) -> Unit
-) :
-    ListAdapter<Todo, TodoAdapter.TodoViewHolder>(DIFF_CALLBACK) {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
-        val viewHolder = TodoViewHolder(ItemTodoBinding.bind(view))
-        viewHolder.binding.checkbox.setOnClickListener {
-            val item = getItem(viewHolder.adapterPosition)
-            item.isDone = !item.isDone
-            clickListener.invoke(item)
-        }
-        return viewHolder
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
-    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        holder.binding.todo = getItem(position)
-        holder.binding.handle.setOnTouchListener { _, event ->
-            if (event.action == MotionEvent.ACTION_DOWN) {
-                dragListener.invoke(holder)
-            }
-            false
-        }
-    }
-
-    class TodoViewHolder(val binding: ItemTodoBinding) : RecyclerView.ViewHolder(binding.root)
-
-    fun getTodo(position: Int): Todo = getItem(position)
-
-    companion object {
-        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Todo>() {
-            override fun areItemsTheSame(oldItem: Todo, newItem: Todo) = oldItem.uid == newItem.uid
-
-            override fun areContentsTheSame(oldItem: Todo, newItem: Todo) = oldItem == newItem
         }
     }
 }
---
+++ app/src/main/java/dev/jsoh/myapplication/ui/TodoAdapter.kt
@@ -0,0 +1,53 @@
+package dev.jsoh.myapplication.ui
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import dev.jsoh.myapplication.R
+import dev.jsoh.myapplication.databinding.ItemTodoBinding
+import dev.jsoh.myapplication.models.Todo
+
+class TodoAdapter(
+    private val clickListener: (item: Todo) -> Unit,
+    private val dragListener: (viewHolder: RecyclerView.ViewHolder) -> Unit
+) :
+    ListAdapter<Todo, TodoAdapter.TodoViewHolder>(DIFF_CALLBACK) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
+        val viewHolder = TodoViewHolder(ItemTodoBinding.bind(view))
+        viewHolder.binding.checkbox.setOnClickListener {
+            val item = getItem(viewHolder.adapterPosition)
+            item.isDone = !item.isDone
+            clickListener.invoke(item)
+        }
+        return viewHolder
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.binding.todo = getItem(position)
+        holder.binding.handle.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                dragListener.invoke(holder)
+            }
+            false
+        }
+    }
+
+    class TodoViewHolder(val binding: ItemTodoBinding) : RecyclerView.ViewHolder(binding.root)
+
+    fun getTodo(position: Int): Todo = getItem(position)
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Todo>() {
+            override fun areItemsTheSame(oldItem: Todo, newItem: Todo) = oldItem.uid == newItem.uid
+
+            override fun areContentsTheSame(oldItem: Todo, newItem: Todo) = oldItem == newItem
+        }
+    }
+}
